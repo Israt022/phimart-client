a40e396039d4c4ec2f71fef093a9598a19940e5f
Israt Jahan <eraisrat022@gmail.com>
2025-05-18 23:36:43 +0600

    React Module-34 Added

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,14 +3,30 @@ import About from "../pages/About";
 import Home from "../pages/Home";
 import MainLayout from "../layouts/MainLayout";
 import Shop from "../pages/Shop";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Dashboard from "../pages/Dashboard";
+import PrivateRoute from "../components/PrivateRoute";
+import ActivateAccount from "../components/Registration/ActivateAccount";
 
 const AppRoutes = () => {
     return (
         <Routes>
+
             <Route element={<MainLayout/>} >
                 <Route path="/" element={<Home />} />
                 <Route path="about" element={<About />} />
                 <Route path="shop" element={<Shop />} />
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+                <Route path="activate/:uid/:token" element={<ActivateAccount />} />
+                <Route 
+                    path="dashboard" 
+                    element = {
+                    <PrivateRoute>
+                        <Dashboard/>
+                    </PrivateRoute>
+                }/>
             </Route>
         </Routes>
     );
